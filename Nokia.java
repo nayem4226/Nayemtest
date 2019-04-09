638d66b28cfcb7c12dc7d76fc6e2705779ad036e
nayeem4226 <nayem.rahman911@gmail.com>
2019-04-09 18:05:29 -0400

    first

---
+++ Nokia.java
@@ -0,0 +1,31 @@
+package newpackage;
+
+public class Nokia {
+	public static void main(String [] args) {
+		String name = "naeymur rahman";
+		int m = 15;
+		int h = 8;
+		
+		int dailytotal = m*h;
+		System.out.println(name+", daily income = "+dailytotal);
+		
+		int wh = 20;
+		int weeklytotal = wh*m;
+		System.out.println(name+", weekly income = "+weeklytotal);
+		
+		int mh = 80;
+		int monthlytotal = mh*m;
+		System.out.println(name+", monthly income = "+monthlytotal);
+		
+		int yh = 960;
+		int yearlytotal = yh*m;
+		System.out.println(name+", yearly income = "+yearlytotal);
+		
+		
+		
+		
+		
+		
+				
+ }
+} 
---
+++ Nokiaexcution.java
@@ -0,0 +1,9 @@
+package newpackage;
+
+public class Nokiaexcution {
+	public static void main(String [] args) {
+		
+		
+	}
+
+}
